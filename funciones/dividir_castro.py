acf7b382884054c37b9d15636d8ec4b66f160573
Nahuel Castro <m.nahucastro2019@gmail.com>
2025-11-04 19:46:16 -0300

    Agregue funcion division con mi apellido

---
+++ funciones/dividir_castro.py
@@ -0,0 +1,12 @@
+#funciones/dividir_castro.py
+
+def dividir_castro(a, b):
+    """Devuelve la división de dos números. Si b es 0, devuelve None."""
+    
+    # Esta línea DEBE estar indentada (con 4 espacios)
+    if b == 0:
+        # Esta línea DEBE estar indentada (con 8 espacios)
+        return None
+    
+    # Esta línea DEBE estar indentada (con 4 espacios)
+    return a / b
