8fce32f768a499305d635d84817f02ad64d90dae
rafael-martinez98 <antoniorafaelmartinezaguero@gmail.com>
2025-11-04 21:37:39 -0300

    integre cambios de arreglo de importacion de funcion

--- tests/testsnumeroparmartinez.py
+++ tests/testsnumeroparmartinez.py
@@ -1,5 +1,6 @@
-tests/test_numeroparmartinez.py
-from funciones.es_par import es_par
+# tests/test_numeropar_martinez.py
+from funciones import es_par
+
 def test_es_par():
     assert es_par(4) is True
-assert es_par(7) is False
+    assert es_par(7) is False
